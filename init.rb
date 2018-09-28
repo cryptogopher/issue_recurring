22dcb4645d24b15aa981fda96dbfbfd20e343aeb
cryptogopher <github@michalczyk.pro>
2018-09-28 21:04:19 +0200

    General plugin info.

--- init.rb
+++ init.rb
@@ -1,8 +1,8 @@
 Redmine::Plugin.register :issue_recurrence do
-  name 'Issue Recurrence plugin'
-  author 'Author name'
-  description 'This is a plugin for Redmine'
+  name 'Issue recurrence plugin'
+  author 'cryptogopher'
+  description 'Schedule Redmine issue recurrence based on multiple conditions'
   version '0.0.1'
-  url 'http://example.com/path/to/plugin'
-  author_url 'http://example.com/about'
+  url 'https://github.com/cryptogopher/issue_recurrence'
+  author_url 'https://github.com/cryptogopher'
 end
