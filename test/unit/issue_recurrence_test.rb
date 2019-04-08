effcc9407cce491ac25881f3c24defd928811e79
cryptogopher <github@michalczyk.pro>
2019-04-08 23:50:02 +0200

    Fixed unit test.

--- test/unit/issue_recurrence_test.rb
+++ test/unit/issue_recurrence_test.rb
@@ -1,13 +1,16 @@
 require File.expand_path('../../test_helper', __FILE__)
 
 class IssueRecurrenceTest < ActiveSupport::TestCase
-  fixtures :issues
+  fixtures :issues, :users
 
   def setup
     @issue1 = issues(:issue_01)
+
+    User.current = users(:alice)
   end
 
   def test_new
+    @issue1.update!(start_date: '2019-04-08', due_date: '2019-04-12')
     ir = IssueRecurrence.new(issue: @issue1)
     assert ir
     ir.save!
