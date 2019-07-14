b438630f7a70664936c9e47cd61ccb1a3b3029e8
cryptogopher <git6453hub@michalczyk.pro>
2019-07-14 14:07:39 +0200

    Version bump to 1.3

--- init.rb
+++ init.rb
@@ -14,7 +14,7 @@ Redmine::Plugin.register :issue_recurring do
   name 'Issue recurring plugin'
   author 'cryptogopher'
   description 'Schedule Redmine issue recurrence based on multiple conditions'
-  version '1.2'
+  version '1.3'
   url 'https://github.com/cryptogopher/issue_recurring'
   author_url 'https://github.com/cryptogopher'
 
