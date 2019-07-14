2fb4026ebab3c18e71889d1bf6f28b45730c3da5
cryptogopher <git6453hub@michalczyk.pro>
2019-07-14 17:43:41 +0200

    Fixed migrations to work with both Rails 4 and 5

--- db/migrate/002_add_recurrence_of_to_issues.rb
+++ db/migrate/002_add_recurrence_of_to_issues.rb
@@ -1,4 +1,6 @@
-class AddRecurrenceOfToIssues < ActiveRecord::Migration[4.2]
+class AddRecurrenceOfToIssues <
+  (Rails::VERSION::MAJOR < 5 ? ActiveRecord::Migration : ActiveRecord::Migration[4.2])
+
   def change
     add_reference :issues, :recurrence_of, index: true
   end
--- db/migrate/004_add_anchor_date_to_issue_recurrence.rb
+++ db/migrate/004_add_anchor_date_to_issue_recurrence.rb
@@ -1,4 +1,6 @@
-class AddAnchorDateToIssueRecurrence < ActiveRecord::Migration[4.2]
+class AddAnchorDateToIssueRecurrence <
+  (Rails::VERSION::MAJOR < 5 ? ActiveRecord::Migration : ActiveRecord::Migration[4.2])
+
   def change
     add_column :issue_recurrences, :anchor_date, :date
   end
